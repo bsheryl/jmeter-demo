7a14662c1dba4390cdbf663563d65b04a3c6d2e8
bsheryl <aleksandr.stepanov.91@list.ru>
2025-03-23 22:06:00 +0400

    +micrometer, prometheus, timed metrics

--- src/main/java/com/example/jmeter/demo/controller/Controller.java
+++ src/main/java/com/example/jmeter/demo/controller/Controller.java
@@ -1,6 +1,8 @@
 package com.example.jmeter.demo.controller;
 
+import com.example.jmeter.demo.model.Message;
 import com.example.jmeter.demo.model.User;
+import io.micrometer.core.annotation.Timed;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,12 +12,19 @@ import java.time.LocalDateTime;
 @RequestMapping("/user")
 public class Controller {
     @GetMapping
-    public ResponseEntity<String> get() {
-        return ResponseEntity.ok("{\"message\": \"response success\"}");
+    @Timed(value = "simple_get_method_execution_time", description = "Time taken to execute simple GET method")
+    public ResponseEntity<Message> get() {
+        Message message = new Message();
+        message.setMessage("success");
+        return ResponseEntity.ok(message);
     }
 
     @PostMapping
-    public ResponseEntity<User> post(@RequestBody User user) {
+    @Timed(value = "simple_post_method_execution_time", description = "Time taken to execute simple POST method")
+    public ResponseEntity<User> post(@RequestBody User user) throws IllegalAccessException{
+        if (user.getLogin().isEmpty() || user.getPassword().isEmpty()) {
+            throw new IllegalArgumentException("Login or password is empty");
+        }
         user.setCurrentDate(LocalDateTime.now());
         return ResponseEntity.ok(user);
     }
---
+++ src/main/java/com/example/jmeter/demo/model/Message.java
@@ -0,0 +1,10 @@
+package com.example.jmeter.demo.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class Message {
+    private String message;
+}
